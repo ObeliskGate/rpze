005e376a85e87aacec2d251c3babedb878f4ff7c
ObeliskGate <3298569723@qq.com>
2023-09-29 14:35:33 +0800

    instance push

---
+++ rp_main/module.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <cmath>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+const double e = 2.7182818284590452353602874713527;
+
+double sinh_impl(double x) {
+    return (1 - pow(e, (-2 * x))) / (2 * pow(e, -x));
+}
+
+double cosh_impl(double x) {
+    return (1 + pow(e, (-2 * x))) / (2 * pow(e, -x));
+}
+
+double tanh_impl(double x) {
+    return sinh_impl(x) / cosh_impl(x);
+}
+
+PYBIND11_MODULE(rp_main, m) {
+    m.def("fast_tanh2", &tanh_impl, R"pbdoc(
+        Compute a hyperbolic tangent of a single argument expressed in radians.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
---
+++ rpze/rpze.py
@@ -0,0 +1,8 @@
+import pywin32_system32 as win
+import rp_main as rp
+
+
+if __name__ == "__main__":  
+    win.FindWindow("Plants vs. Zombies")
+    print(rp.fast_tanh2(45.0))
+    print("hello")
