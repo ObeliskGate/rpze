35ca085a10f72226e59ab3d8707bb3217d6393bb
ObeliskGate <3298569723@qq.com>
2023-09-30 17:15:49 +0800

    instance push

---
+++ rpze/inject.py
@@ -0,0 +1,20 @@
+import win32api as win
+import win32gui as wing
+import win32process as winp
+import os
+
+def find_window(window_name) -> int:
+    """
+    返回名称为str的窗口主进程pid
+    """
+    handle = wing.FindWindow(None, window_name)
+    _, pid = winp.GetWindowThreadProcessId(handle)
+    return pid
+
+def inject(pids : list):
+    s = f'..\\Release\\rp_injector.exe \"C:\\space\\projects\\rpz\\Release\\rp_dll.dll\" {len(pids)} '
+    for i in pids:
+        s += str(i)
+        s += ' '
+     
+    os.system(s)
