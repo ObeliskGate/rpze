42d356897463eeacc9ba712ca4637d3c2adfe428
ObeliskGate <unnamets@gmail.com>
2023-10-15 16:17:29 +0800

    small fixes

--- rpze/tests.py
+++ rpze/tests.py
@@ -1,26 +1,8 @@
 ﻿import msvcrt as vc
 import time
 from rp_extend import Controller
-import basic.asm as asm
 import structs.plant as plt
 import structs.zombie as zmb
-
-
-def izombie_place_zombie(x: int, y: int, _type: int, ctler: Controller):
-    p_challenge = ctler.read_i32([0x6a9ec0, 0x768, 0x160])
-    code = f'''
-        push edx;
-        mov eax, {y};
-        push {x};
-        push {_type};
-        mov ecx, {p_challenge};
-        mov edx, 0x42a0f0;
-        call edx;
-        pop edx;
-        ret;'''
-    print(code)
-    asm.run(code, ctler)
-    return ctler.result_i32
 
 
 def basic_test(controller: Controller):
@@ -67,13 +49,12 @@ def asm_and_plant_test(ctler):
     print(plant.__repr__())
     plist: plt.PlantList = plt.get_plant_list(ctler)
 
-    for p in [p for p in plist if not p.is_dead]:
+    for p in plist:
         print(p)
 
 
 def zombie_list_test(ctler):
     zlist: zmb.ZombieList = zmb.get_zombie_list(ctler)
-    p = zlist.__getitem__(2)
 
     print(zlist)
 
