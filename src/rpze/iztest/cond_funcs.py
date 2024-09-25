698ae9437f52bab2cdfabfd4dbf92b5c2bef027a
ivan216 <644767800@qq.com>
2024-09-25 13:42:45 +0800

    Update cond_funcs.py

--- src/rpze/iztest/cond_funcs.py
+++ src/rpze/iztest/cond_funcs.py
@@ -68,3 +68,26 @@ def until_plant_last_shoot(plant: Plant, wait_until_150: bool = False) -> Awaita
         return False
 
     return AwaitableCondFunc(_await_func)
+
+def until_plant_n_shoot(plant: Plant, n:int = 1, non_stop :bool = True) -> AwaitableCondFunc[None]:
+    """
+    生成一个 等到植物n次攻击 的函数
+
+    Args:
+        plant: 要判断的植物
+        n: 攻击次数
+        non_stop: 是否为不间断攻击
+    """
+    def _await_func(fm: FlowManager,
+                v=VariablePool(try_to_shoot_time=None, shots = 0)):
+        if plant.generate_cd == 1:  # 下一帧开打
+            v.try_to_shoot_time = fm.time + 1
+        if v.try_to_shoot_time == fm.time and plant.launch_cd != 0:  # 在攻击时
+            v.shots += 1
+        if v.try_to_shoot_time == fm.time and plant.launch_cd == 0: #不再攻击时
+            if non_stop:    #设置了不停止标志，则计数清零
+                v.shots = 0
+        if v.shots == n:
+            return True
+        return False
+    return AwaitableCondFunc(_await_func)
