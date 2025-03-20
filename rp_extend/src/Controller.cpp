f2377b1b3e5abb326292ffc261937436f97d7392
ObeliskGate <unnamets@gmail.com>
2025-03-20 10:16:01 +0800

    refactor: use macro to generate bindings

--- rp_extend/src/Controller.cpp
+++ rp_extend/src/Controller.cpp
@@ -1,6 +1,19 @@
 #include "Controller.h"
 #include "MemoryException.h"
 
+#define RP_REPEAT_MACRO(macro) \
+	macro(bool, bool) \
+	macro(i8, int8_t) \
+	macro(i16, int16_t) \
+	macro(i32, int32_t) \
+	macro(i64, int64_t) \
+	macro(u8, uint8_t) \
+	macro(u16, uint16_t) \
+	macro(u32, uint32_t) \
+	macro(u64, uint64_t) \
+	macro(f32, float) \
+	macro(f64, double)
+
 PYBIND11_MODULE(rp_extend, m)
 {
 	py::enum_<HookPosition>(m, "HookPosition")
@@ -49,46 +62,28 @@ PYBIND11_MODULE(rp_extend, m)
 		.def_property("jumping_sync_method", &Controller::jumping_sync_method, &Controller::set_jumping_sync_method)
 
 		// read
-		 .def("read_bool", &Controller::read_memory<bool>, py::arg("force_remote") = false)
-        .def("read_i8", &Controller::read_memory<int8_t>, py::arg("force_remote") = false)
-        .def("read_i16", &Controller::read_memory<int16_t>, py::arg("force_remote") = false)
-        .def("read_i32", &Controller::read_memory<int32_t>, py::arg("force_remote") = false)
-        .def("read_i64", &Controller::read_memory<int64_t>, py::arg("force_remote") = false)
-        .def("read_u8", &Controller::read_memory<uint8_t>, py::arg("force_remote") = false)
-        .def("read_u16", &Controller::read_memory<uint16_t>, py::arg("force_remote") = false)
-        .def("read_u32", &Controller::read_memory<uint32_t>, py::arg("force_remote") = false)
-        .def("read_u64", &Controller::read_memory<uint64_t>, py::arg("force_remote") = false)
-        .def("read_f32", &Controller::read_memory<float>, py::arg("force_remote") = false)
-        .def("read_f64", &Controller::read_memory<double>, py::arg("force_remote") = false)
+#define RP_READ_MODEL(t_name, t) \
+		.def("read_"#t_name, &Controller::read_memory<t>, py::arg("force_remote") = false)
+
+		RP_REPEAT_MACRO(RP_READ_MODEL)
+
         .def("read_bytes", &Controller::read_bytes, py::arg("size"), py::arg("force_remote") = false)
 
 		// write
-        .def("write_bool", &Controller::write_memory<bool>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_i8", &Controller::write_memory<int8_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_i16", &Controller::write_memory<int16_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_i32", &Controller::write_memory<int32_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_i64", &Controller::write_memory<int64_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_u8", &Controller::write_memory<uint8_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_u16", &Controller::write_memory<uint16_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_u32", &Controller::write_memory<uint32_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_u64", &Controller::write_memory<uint64_t>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_f32", &Controller::write_memory<float>, py::arg("value"), py::arg("force_remote") = false)
-        .def("write_f64", &Controller::write_memory<double>, py::arg("value"), py::arg("force_remote") = false)
+#define RP_WRITE_MODEL(t_name, t) \
+		.def("write_"#t_name, &Controller::write_memory<t>, py::arg("value"), py::arg("force_remote") = false)
+			
+		RP_REPEAT_MACRO(RP_WRITE_MODEL)
+		
 		.def("write_bytes", &Controller::write_bytes, py::arg("value"), py::arg("force_remote") = false)
 
 		.def_property_readonly("result_address", &Controller::result_address)
 		.def_property_readonly("asm_address", &Controller::asm_address)
-		.def_property("result_bool", &Controller::get_result<bool>, &Controller::set_result<bool>)
-		.def_property("result_i8", &Controller::get_result<int8_t>, &Controller::set_result<int8_t>)
-		.def_property("result_i16", &Controller::get_result<int16_t>, &Controller::set_result<int16_t>)
-		.def_property("result_i32", &Controller::get_result<int32_t>, &Controller::set_result<int32_t>)
-		.def_property("result_i64", &Controller::get_result<int64_t>, &Controller::set_result<int64_t>)
-		.def_property("result_u8", &Controller::get_result<uint8_t>, &Controller::set_result<uint8_t>)
-		.def_property("result_u16", &Controller::get_result<uint16_t>, &Controller::set_result<uint16_t>)
-		.def_property("result_u32", &Controller::get_result<uint32_t>, &Controller::set_result<uint32_t>)
-		.def_property("result_u64", &Controller::get_result<uint64_t>, &Controller::set_result<uint64_t>)
-		.def_property("result_f32", &Controller::get_result<float>, &Controller::set_result<float>)
-		.def_property("result_f64", &Controller::get_result<double>, &Controller::set_result<double>);
+
+#define RP_RESULT_MODEL(t_name, t) \
+		.def_property("result_"#t_name, &Controller::get_result<t>, &Controller::set_result<t>)
+
+		RP_REPEAT_MACRO(RP_RESULT_MODEL);
 
 }
 
